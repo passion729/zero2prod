589b6fc24e900c22b271cc9a17fc5714c944756c
Passion <oscarpassion36@gmail.com>
2025-03-28 21:11:56 +0800

    refactor: optimize health check api & add tests

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,9 @@
-use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
+use std::net::TcpListener;
+use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind("127.0.0.1:8080")?
-        .run()
-        .await
-}
-
-async fn health_check() -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[tokio::test]
-    async fn health_check_succeeds() {
-        let response = health_check().await;
-        assert!(response.status().is_success())
-    }
+    let listener = TcpListener::bind("127.0.0.1:8000")
+        .expect("Unable to bind TCP listener");
+    run(listener)?.await
 }
