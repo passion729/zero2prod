7b93344cff6338c2388224aab213f75bc33b4e0b
Passion <oscarpassion36@gmail.com>
2025-03-31 19:14:40 +0800

    chore: add dockerfile

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,8 +1,7 @@
 use once_cell::sync::Lazy;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
-use secrecy::ExposeSecret;
-use zero2prod::configuration::{DataBaseSettings, get_configuration};
+use zero2prod::configuration::{get_configuration, DataBaseSettings};
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
@@ -112,7 +111,7 @@ async fn spawn_app() -> TestApp {
 }
 
 async fn configure_database(config: &DataBaseSettings) -> PgPool {
-    let mut connection = PgConnection::connect(&config.connection_string_without_db().expose_secret())
+    let mut connection = PgConnection::connect_with(&config.without_db())
         .await
         .expect("Failed to connect to Postgres.");
     connection
@@ -120,7 +119,7 @@ async fn configure_database(config: &DataBaseSettings) -> PgPool {
         .await
         .expect("Failed to create database.");
 
-    let connection_pool = PgPool::connect(&config.connection_string().expose_secret())
+    let connection_pool = PgPool::connect_with(config.with_db())
         .await
         .expect("Failed to connect to Postgres.");
     sqlx::migrate!("./migrations")
